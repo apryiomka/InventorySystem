fd12249c6d8a50bb4dbfe697cec7cdebf45aa5e4
Alex Pryiomka <aliaksandrp@zillowgroup.com>
2019-10-05 18:10:21 -0700

    Clean up the repo

--- src/InventorySyctem.Services/Bus/IMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace inventorySyctem.Services.Bus
-{
-    /// <summary>
-    /// The service bus message
-    /// </summary>
-    public interface IMessage { }
-}
--- src/inventorySystem.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="inventorySystem.Web.WebApiApplication" Language="C#" %>
